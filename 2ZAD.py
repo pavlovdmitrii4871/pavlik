15a134e64d88f6b7827ff0e648bc80a9ea3f9bc9
pavlovdmitrii4871 <59319885+pavlovdmitrii4871@users.noreply.github.com>
2024-02-28 11:48:27 +0300

    Update 2ZAD.py

--- 2ZAD.py
+++ 2ZAD.py
@@ -10,19 +10,19 @@ def generateRandomMatrix(m=None, n=None, min_limit=0, max_limit=100):
     for i in range(m):
         row = []
         for j in range(n):
-            num = random.randint(min_limit, max_limit)  # случайное значение в заданном диапазоне
+            num = random.randint(min_limit, max_limit)
             row.append(num)
         matrix.append(row)
 
     return matrix
 
 
-m = int(input("Введите количество строк матрицы: "))
-n = int(input("Введите количество столбцов матрицы: "))
-min_limit = int(input("Введите минимальное значение: "))
-max_limit = int(input("Введите максимальное значение: "))
+m = int(input("Количество строк в матрице: "))
+n = int(input("Количество столбцов в матрице: "))
+min_limit = int(input("Минимальное значение элементов в матрице: "))
+max_limit = int(input("Максимальное значение элементов в матрице: "))
 
 random_matrix = generateRandomMatrix(m, n, min_limit, max_limit)
-print("Матрица:")
+print("Результат: ")
 for row in random_matrix:
     print(row)
